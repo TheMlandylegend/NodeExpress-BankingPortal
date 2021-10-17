fabcfb6250b5cb580dfecfaea80442aa8984b669
mlandy09 <mc_louigene@yahoo.com>
2021-10-17 09:28:50 -0400

    Module 5

--- src/data.js
+++ src/data.js
@@ -1,2 +1,5 @@
 const fs = require('fs');
 const path = require('path');
+
+const accountData = fs.readFileSync(path.join(__dirname, 'json', 'accounts.json'), 'utf8');
+const accounts = JSON.parse(accountData);
