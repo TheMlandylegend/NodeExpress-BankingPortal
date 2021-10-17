e5de08f798e4bfef799904306d2866864b3ff28a
mlandy09 <mc_louigene@yahoo.com>
2021-10-17 09:48:07 -0400

    Module 5

--- src/data.js
+++ src/data.js
@@ -11,7 +11,7 @@ const userData = fs.readFileSync(path.join(__dirname, 'json', 'users.json'), 'ut
 const users = JSON.parse(userData);
 
 //// write JSON FUnction
-const writeJSOn =() => {
+const writeJSON =() => {
     const accountsJSON = JSON.stringify(accounts, null, 4);
     fs.writeFileSync(path.join(__dirname, 'json/accounts.json'), accountsJSON, 'utf8');
 };
