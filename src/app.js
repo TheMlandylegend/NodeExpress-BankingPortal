7961011af98b717b68e5d14aba81135824349229
mlandy09 <mc_louigene@yahoo.com>
2021-10-17 08:50:38 -0400

    Module 3

--- src/app.js
+++ src/app.js
@@ -3,6 +3,7 @@ const fs = require('fs');
 const path = require('path');
 
 const express = require('express');
+const { __ } = require('ramda');
 const app = express();
 
 app.set('views', path.join(__dirname, 'views'));
@@ -25,9 +26,26 @@ app.get('/checking', (req, res) => res.render('account', { account: accounts.che
 app.get('/credit', (req, res) => res.render('account', { account: accounts.credit }));
 
 app.get('/transfer', (req, res) => res.render('transfer'));
-// app.post('/transfer', (req, res) => {
+app.post('/transfer', (req, res) => {
+    accounts[req.body.from].balance = accounts[req.body.from].balance -
+    req.body.ammount;
+    accounts[req.body.to].balance = parseInt(accounts[req.body.to].balance) +
+    parseInt(req.body.amount, 10);
+    const accountsJSON = JSON.stringify(accounts, null, 4);
+    fs.writeFileSync(path.join(__dirname, 'json/accounts.json'), accountsJSON, 'utf8');
+    res.render('transfer', {message: 'Transfer Completed'});   
+});
 
-// });
+app.get('/payment', (req, res) => res.render('payment', {account: accounts.credit} ));
+
+app.post('/payment', (req, res) =>{
+    accounts.credit.balance -= req.body.amount;
+    accounts.credit.available += parseInt(req.body.amount, 10);
+    const accountsJSON = JSON.stringify(accounts, null, 4);
+    fs.writeFileSync(path.join(__dirname, 'json', 'accounts.json'), accountsJSON,
+    'utf8');
+    res.render('payment', {message: 'Payment Successful', account: accounts.credit});
+});
 
 app.get('/profile', (req, res) =>  res.render('profile', { user: users[0] }));
 
