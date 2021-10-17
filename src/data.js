b78bdb382213164b01bdc8f87e5a4e75f84d2f79
mlandy09 <mc_louigene@yahoo.com>
2021-10-17 09:30:42 -0400

    Module 5

--- src/data.js
+++ src/data.js
@@ -1,5 +1,11 @@
 const fs = require('fs');
 const path = require('path');
 
+//Resonsile for reading and parsing JSON form the accounts.json file
 const accountData = fs.readFileSync(path.join(__dirname, 'json', 'accounts.json'), 'utf8');
 const accounts = JSON.parse(accountData);
+
+//Resonsile for reading and parsing JSON form the Users.json file
+
+const userData = fs.readFileSync(path.join(__dirname, 'json', 'users.json'), 'utf8');
+const users = JSON.parse(userData);
