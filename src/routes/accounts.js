ec73340380fce6e6f1fc39d7551832bbd40c728c
mlandy09 <mc_louigene@yahoo.com>
2021-10-17 13:49:25 -0400

    Module 6

--- src/routes/accounts.js
+++ src/routes/accounts.js
@@ -1,5 +1,6 @@
-const fs = require('fs');
-const path = require('path');
+//const fs = require('fs');
+//const path = require('path');
 const express = require('express');
-const { __ } = require('ramda');
-const app = express();
+
+const router = express.Router();
+const{accounts} = require('../data');
